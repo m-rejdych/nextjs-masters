47e0c7b2db9778dc83bf6a896bbe31f5caeef0f2
m-rejdych <rejdych.m.r@gmail.com>
2024-02-09 08:31:44 +0100

    Initial eslint, prettier configuration.

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	trailingComma: 'all',
+	printWidth: 100,
+	useTabs: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+	tailwindConfig: './tailwind.config.ts',
+};
