7627d48e0b55613f71f7f553157e97b804bb0432
m-rejdych <rejdych.m.r@gmail.com>
2024-02-09 17:52:14 +0100

    Added data-testid.

--- client/src/ui/molecules/ProductsList.tsx
+++ client/src/ui/molecules/ProductsList.tsx
@@ -6,7 +6,10 @@ interface Props {
 }
 
 export const ProductsList = ({ products }: Props) => (
-	<ul className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+	<ul
+    className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8"
+    data-testid="products-list"
+  >
 		{products.map(({ id, ...rest }) => (
 			<ProductListItem key={id} {...rest} />
 		))}
