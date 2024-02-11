86e47631eb0f0909d6fb8b245cc11b1736626bee
m-rejdych <rejdych.m.r@gmail.com>
2024-02-11 21:32:13 +0100

    Added initial Apollo config.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,29 @@
-Bun.serve({
-	port: Bun.env.PORT,
-	fetch: () => {
-		return new Response(`Hi thre`);
-	},
+import path from 'path';
+import { readFileSync } from 'fs';
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import type { Resolvers } from './generated/graphql';
+import type { Context } from './graphql/context';
+
+const typeDefs = readFileSync(path.join(__dirname, 'graphql', 'schema.graphql'), {
+	encoding: 'utf-8',
 });
 
-console.log(`Listening on http://localhost:${Bun.env.PORT}`);
+const resolvers: Resolvers = {
+  Query: {
+    hello: () => 'hi',
+  }
+};
+
+const server = new ApolloServer<Context>({
+	typeDefs,
+	resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+	listen: { port: parseInt(Bun.env.PORT, 10) },
+  context: async (): Promise<Context> => ({}),
+});
+
+console.log(`Listening on ${url}`);
