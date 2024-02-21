009fdc9c6dfe5928c1972d07b5378d319c1781f3
m-rejdych <rejdych.m.r@gmail.com>
2024-02-21 19:37:03 +0100

    Added product pages static generation.

--- client/src/app/products/[currentPage]/page.tsx
+++ client/src/app/products/[currentPage]/page.tsx
@@ -3,11 +3,36 @@ import { ProductsList } from '@/ui/molecules/products/ProductsList';
 import { Pagination } from '@/ui/molecules/nav/Pagination';
 import { getProducts } from '@/api/products';
 
-interface Props {
-	params: {
-		currentPage: string;
-	};
+interface Params {
+	currentPage: string;
 }
+
+interface Props {
+	params: Params;
+}
+
+export const generateStaticParams = async () => {
+	let hasMore = true;
+	let currentPage = 1;
+	const maxPagesCount = 20;
+	const params: Params[] = [];
+
+	while (hasMore && currentPage <= maxPagesCount) {
+		const products = await getProducts(20, (currentPage - 1) * 20);
+		if (!products) {
+			hasMore = false;
+		} else {
+			params.push({ currentPage: currentPage.toString() });
+			if (products.length < 20) {
+				hasMore = false;
+			} else {
+				currentPage++;
+			}
+		}
+	}
+
+	return params;
+};
 
 export default async function ProductsPaginated({ params: { currentPage } }: Props) {
 	const products = await getProducts(20, (Number(currentPage) - 1) * 20);
