8594fb9377541c862ef0b0a01b66ae500911f312
m-rejdych <rejdych.m.r@gmail.com>
2024-03-22 02:35:14 +0100

    Fixed server domain for images.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
 			{ hostname: 'fakestoreapi.com', protocol: 'https' },
 			{ hostname: 'cdn.dummyjson.com', protocol: 'https' },
 			{ hostname: 'localhost', protocol: 'http' },
-			{ hostname: 'nextjs-shop-api.mrejdych', protocol: 'https' },
+			{ hostname: 'nextjs-shop-api.mrejdych.com', protocol: 'https' },
 		],
 	},
 	async redirects() {
